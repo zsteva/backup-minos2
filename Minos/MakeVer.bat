aabfb92c48a6a143c76544742231f03019235fd8
g0gjv <mike@goodey.org.uk>
2014-10-03 18:44:56 +0100

    Make work on 64 bit; 2014

--- Minos/MakeVer.bat
+++ Minos/MakeVer.bat
@@ -1,6 +1,9 @@
 REM CommandInterpreter: $(COMSPEC)
-REM cd ..\adjsql
+if exist C:\"Program Files"\Git\bin\git ( 
 C:\"Program Files"\Git\bin\git rev-parse HEAD >gitver.in
+) else (
+  C:\"Program Files (x86)"\Git\bin\git rev-parse HEAD >gitver.in
+)
 embedGit\embedGitVersion gitver.in gitver.h
 del MinosVer.res
 brcc32 MinosVer.rc
