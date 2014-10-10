66686fa59f9bd99b3369966922c52abb43b4c0a4
mike <mike@goodey.org.uk>
2014-10-10 16:35:49 +0100

    More on 32/64 bit versioning

--- Minos/MakeVer.bat
+++ Minos/MakeVer.bat
@@ -1,8 +1,8 @@
 REM CommandInterpreter: $(COMSPEC)
-if exist C:\"Program Files"\Git\bin\git ( 
-C:\"Program Files"\Git\bin\git rev-parse HEAD >gitver.in
+if exist C:\"Program Files"\Git\bin\git.exe (
+C:\"Program Files"\Git\bin\git.exe rev-parse HEAD >gitver.in
 ) else (
-  C:\"Program Files (x86)"\Git\bin\git rev-parse HEAD >gitver.in
+  C:\"Program Files \(x86\)"\Git\bin\git.exe rev-parse HEAD >gitver.in
 )
 embedGit\embedGitVersion gitver.in gitver.h
 del MinosVer.res
