ee4e50567e873ca714e15996b591ccacd45cb256
g0gjv <mike@goodey.org.uk>
2016-01-26 14:09:02 +0000

    File name capitalisation

--- mqt/MqtLogger/tlogcontainer.cpp
+++ mqt/MqtLogger/tlogcontainer.cpp
@@ -439,7 +439,7 @@ void TLogContainer::FileNewActionExecute()
     char letter = 'A';
     while ( letter < 'Z' )      // the A of A.Minos
     {
-       QString fileNameBuff = InitialDir + "/" + nfileName + letter + ".minos";
+       QString fileNameBuff = InitialDir + "/" + nfileName + letter + ".Minos";
 
        if (FileExists(fileNameBuff))
        {
@@ -449,7 +449,7 @@ void TLogContainer::FileNewActionExecute()
           break;
     }
 
-    QString initName = InitialDir + nfileName + letter + ".minos";
+    QString initName = InitialDir + nfileName + letter + ".Minos";
     ContestDetails pced( this );
     BaseContestLog * c = addSlot( &pced, initName, true, -1 );
 
@@ -523,7 +523,7 @@ void TLogContainer::FileOpenActionExecute()
     // first choose file
 //"Images (*.png *.xpm *.jpg);;Text files (*.txt);;XML files (*.xml)"
 
-    QString Filter = "Minos contest files (*.minos);;"
+    QString Filter = "Minos contest files (*.Minos);;"
                      "Reg1Test Files (*.edi);;"
                      "GJV contest files (*.gjv);;"
                      "RSGB Log Files (*.log);;"
