0fb6b6abaa462ffb19b6b9e66b2df056b9d38462
mike <mike@goodey.org.uk>
2016-11-25 17:47:03 +0000

    Get git SHA1 working again

--- Minos/MakeVer.bat
+++ Minos/MakeVer.bat
@@ -1,9 +1,10 @@
 REM CommandInterpreter: $(COMSPEC)
-if exist C:\"Program Files"\Git\bin\git.exe (
-C:\"Program Files"\Git\bin\git.exe rev-parse HEAD >gitver.in
+if exist C:/"Program Files"/Git/bin/git.exe (
+"C:\Program Files\Git\bin\git.exe" rev-parse HEAD >gitver.in
+
 goto 1
 )
-C:\"Program Files (x86)"\Git\bin\git.exe rev-parse HEAD >gitver.in
+"C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD >gitver.in
 :1
 
 embedGit\embedGitVersion gitver.in gitver.h
