00792cb37cbb592d3af150bc50e9832480ad00e9
g0gjv <mike@goodey.org.uk>
2017-02-14 09:33:55 +0000

    Volume code, not working

--- mqt/mqtKeyer/AlsaVolume.cpp
+++ mqt/mqtKeyer/AlsaVolume.cpp
@@ -21,20 +21,44 @@ void AlsaVolume::SetAlsaPlaybackMasterVolume(long volume)
     const char *card = "default";
     const char *selem_name = "Master";
 
-    snd_mixer_open(&handle, 0);
-    snd_mixer_attach(handle, card);
-    snd_mixer_selem_register(handle, NULL, NULL);
-    snd_mixer_load(handle);
+    int res;
 
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+    res = snd_mixer_open(&handle, 0);     // return 0 or -ve for error; param is mode
+    if (res < 0)
+        trace(QString("snd_mixer_open ") + snd_strerror(res));
+    res = snd_mixer_attach(handle, card);// atach ctl to mixer
+    if (res < 0)
+        trace(QString("snd_mixer_attach ") + snd_strerror(res));
+    res = snd_mixer_selem_register(handle, NULL, NULL);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_register ") + snd_strerror(res));
+    res = snd_mixer_load(handle);
+    if (res < 0)
+        trace(QString("snd_mixer_load ") + snd_strerror(res));
 
-    snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
-    snd_mixer_selem_set_playback_volume_all(elem, volume * max / 100);
+    res = snd_mixer_selem_id_alloca(&sid);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_id_alloca ") + snd_strerror(res));
+    res = snd_mixer_selem_id_set_index(sid, 0);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_id_set_index ") + snd_strerror(res));
+    res = snd_mixer_selem_id_set_name(sid, selem_name);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_id_set_name ") + snd_strerror(res));
+    res = snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+    if (res < 0)
+        trace(QString("snd_mixer_find_selem ") + snd_strerror(res));
 
-    snd_mixer_close(handle);
+    res = snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_get_playback_volume_range ") + snd_strerror(res));
+    res = snd_mixer_selem_set_playback_volume_all(elem, volume * max / 100);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_set_playback_volume_all ") + snd_strerror(res));
+
+    res = snd_mixer_close(handle);
+    if (res < 0)
+        trace(QString("snd_mixer_close") + snd_strerror(res));
 #else
     Q_UNUSED(volume)
 #endif
@@ -48,20 +72,43 @@ void AlsaVolume::SetAlsaRecordMasterVolume(long volume)
     const char *card = "default";
     const char *selem_name = "Master";
 
-    snd_mixer_open(&handle, 0);
-    snd_mixer_attach(handle, card);
-    snd_mixer_selem_register(handle, NULL, NULL);
-    snd_mixer_load(handle);
+    int res;
+    res = snd_mixer_open(&handle, 0);
+    if (res < 0)
+        trace(QString("snd_mixer_open ") + snd_strerror(res));
+    res = snd_mixer_attach(handle, card);
+    if (res < 0)
+        trace(QString("snd_mixer_attach ") + snd_strerror(res));
+    res = snd_mixer_selem_register(handle, NULL, NULL);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_register ") + snd_strerror(res));
+    res = snd_mixer_load(handle);
+    if (res < 0)
+        trace(QString("snd_mixer_load ") + snd_strerror(res));
 
-    snd_mixer_selem_id_alloca(&sid);
-    snd_mixer_selem_id_set_index(sid, 0);
-    snd_mixer_selem_id_set_name(sid, selem_name);
-    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+    res = snd_mixer_selem_id_alloca(&sid);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_id_alloca ") + snd_strerror(res));
+    res = snd_mixer_selem_id_set_index(sid, 0);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_id_set_index ") + snd_strerror(res));
+    res = snd_mixer_selem_id_set_name(sid, selem_name);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_id_set_name ") + snd_strerror(res));
+    res = snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+    if (res < 0)
+        trace(QString("snd_mixer_find_selem ") + snd_strerror(res));
 
-    snd_mixer_selem_get_capture_volume_range(elem, &min, &max);
-    snd_mixer_selem_set_capture_volume_all(elem, volume * max / 100);
+    res = snd_mixer_selem_get_capture_volume_range(elem, &min, &max);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_get_capture_volume_range ") + snd_strerror(res));
+    res = snd_mixer_selem_set_capture_volume_all(elem, volume * max / 100);
+    if (res < 0)
+        trace(QString("snd_mixer_selem_set_capture_volume_all ") + snd_strerror(res));
 
-    snd_mixer_close(handle);
+    res = snd_mixer_close(handle);
+    if (res < 0)
+        trace(QString("snd_mixer_close ") + snd_strerror(res));
 #else
     Q_UNUSED(volume)
 #endif
