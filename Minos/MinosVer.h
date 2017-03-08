a91d1ef3e4e94d55d45874f6b6abb70d8915d26b
g0gjv <mike@goodey.org.uk>
2017-03-08 15:04:47 +0000

    1.6.1 for release

--- Minos/MinosVer.h
+++ Minos/MinosVer.h
@@ -3,20 +3,20 @@
 //
 // PROJECT NAME 		Minos Amateur Radio Control and Logging System
 //
-// COPYRIGHT         (c) M. J. Goodey G0GJV 2005 - 2015
+// COPYRIGHT         (c) M. J. Goodey G0GJV 2005 - 2017
 //
 /////////////////////////////////////////////////////////////////////////////
 #define MINOSVERSIONMAJ 1
 #define MINOSVERSIONMIN 6
-#define MINOSINCREMENT 0
-#define MINOSVERSION "1.6"
+#define MINOSINCREMENT 1
+#define MINOSVERSION "1.6.1"
 #define MINOSFILEDESCRIPTION "Minos\0"
 #define MINOSINTERNALNAME "Minos\0"
 #define MINOSORIGINALFILENAME "Minos~.exe\0"
 
 #define MINOSBUILD "0"
 #define MINOSBUILDNO 0
-#define MINOSVERSIONBUILD "1.6.0.0"
-#define MINOSRCVERSION "1.6.0.0\0"
+#define MINOSVERSIONBUILD "1.6.1.0"
+#define MINOSRCVERSION "1.6.1.0\0"
 
 // #define BETA_VERSION
