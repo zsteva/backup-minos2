968d0f7c3234c93b3690b88843e9258869fe1ae2
g0gjv <mike@goodey.org.uk>
2017-03-09 11:44:51 +0000

    1.6.2 for release

--- Minos/MinosVer.h
+++ Minos/MinosVer.h
@@ -8,15 +8,15 @@
 /////////////////////////////////////////////////////////////////////////////
 #define MINOSVERSIONMAJ 1
 #define MINOSVERSIONMIN 6
-#define MINOSINCREMENT 1
-#define MINOSVERSION "1.6.1"
+#define MINOSINCREMENT 2
+#define MINOSVERSION "1.6.2"
 #define MINOSFILEDESCRIPTION "Minos\0"
 #define MINOSINTERNALNAME "Minos\0"
 #define MINOSORIGINALFILENAME "Minos~.exe\0"
 
 #define MINOSBUILD "0"
 #define MINOSBUILDNO 0
-#define MINOSVERSIONBUILD "1.6.1.0"
-#define MINOSRCVERSION "1.6.1.0\0"
+#define MINOSVERSIONBUILD "1.6.2.0"
+#define MINOSRCVERSION "1.6.2.0\0"
 
 // #define BETA_VERSION
