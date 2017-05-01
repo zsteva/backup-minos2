df1e9adb1bb5684d8712c4a175cb7ef5436ff770
Mike Goodey <mike@goodey.org.uk>
2017-05-01 14:59:25 +0100

    Tools that need installing for Linux. Run as sudo ./tools.sh

---
+++ tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install cmake -y
+apt-get install build-essential qt5-default qtcreator -y
+apt-get install qtmultimedia5-dev libqt5multimediawidgets5 libqt5multimedia5-plugins libqt5multimedia5 -y
+apt-get install libqt5serialport5 -y
+apt-get install libqt5serialport5-dev -y
+apt-get install git -y
+apt-get install git-gui -y
+
+
+apt-get autoremove -y
+
