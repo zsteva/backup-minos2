4351f17dad849e946b9e8b5ee6b4ecf433e833bc
g0gjv <mike@goodey.org.uk>
2017-07-31 21:03:01 +0100

    Make the message dialog wider

--- mqt/MqtBase/MShowMessageDlg.cpp
+++ mqt/MqtBase/MShowMessageDlg.cpp
@@ -29,10 +29,13 @@ void mShowMessage( const QString &mess, QWidget *Owner )
     QMessageBox msgBox(Owner);
     msgBox.setText( mess );
 
-    QSpacerItem* horizontalSpacer = new QSpacerItem(Owner->width()*2/3, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
-    QGridLayout* layout = dynamic_cast<QGridLayout*>(msgBox.layout());
-    if (layout)
-        layout->addItem(horizontalSpacer, layout->rowCount(), 0, 1, layout->columnCount());
+    if (Owner)
+    {
+        QSpacerItem* horizontalSpacer = new QSpacerItem(Owner->width()*2/3, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+        QGridLayout* layout = dynamic_cast<QGridLayout*>(msgBox.layout());
+        if (layout)
+            layout->addItem(horizontalSpacer, layout->rowCount(), 0, 1, layout->columnCount());
+    }
     msgBox.exec();
 }
 
