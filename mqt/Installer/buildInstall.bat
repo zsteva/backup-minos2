c2854e0ee2bd05155fab1798f4a23932f233f127
Mike Goodey <mike@goodey.org.uk>
2017-10-24 20:51:56 +0100

    Move to Qt 5.9.2 preparatory for release

--- mqt/Installer/buildInstall.bat
+++ mqt/Installer/buildInstall.bat
@@ -13,15 +13,15 @@ mkdir Lists
 mkdir Logs
 mkdir Bin
 
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtAppStarter\release\MqtAppStarter.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtChat\release\MqtChat.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtControl\release\MqtControl.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtKeyer\release\MqtKeyer.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtLogger\release\MqtLogger.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtMonitor\release\MqtMonitor.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtRigControl\release\MqtRigControl.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtRotator\release\MqtRotator.exe Bin
-copy C:\Projects\sfMinos\QTbuild\gcc\563\mqt\MqtServer\release\MqtServer.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtAppStarter\release\MqtAppStarter.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtChat\release\MqtChat.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtControl\release\MqtControl.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtKeyer\release\MqtKeyer.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtLogger\release\MqtLogger.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtMonitor\release\MqtMonitor.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtRigControl\release\MqtRigControl.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtRotator\release\MqtRotator.exe Bin
+copy C:\Projects\sfMinos\QTbuild\gcc\592\mqt\MqtServer\release\MqtServer.exe Bin
 
 copy C:\Projects\hamlib-w32-3.1\bin\*.dll Bin
 
@@ -35,15 +35,15 @@ cd Configuration
 
 cd ../Bin
 
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtAppStarter.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtChat.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtControl.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtKeyer.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtLogger.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtMonitor.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtRigControl.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtRotator.exe
-C:\Qt\5.6.3\mingw49_32\bin\windeployqt.exe MqtServer.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtAppStarter.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtChat.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtControl.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtKeyer.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtLogger.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtMonitor.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtRigControl.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtRotator.exe
+C:\Qt\5.9.2\mingw53_32\bin\windeployqt.exe MqtServer.exe
 
 cd ../..
 mkdir Installer
